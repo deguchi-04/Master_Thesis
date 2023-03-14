dab24fd5fd0a181ec6f9ca5a7edccc1452bf5e97
deguchi-04 <up201802575@fe.up.pt>
2023-03-14 22:51:07 +0000

    deploy with depthai

---
+++ software_spray/build/CMakeFiles/dep.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dep.dir/src/cnn_with_depthai.cpp.o"
+  "CMakeFiles/dep.dir/src/cnn_with_depthai.cpp.o.d"
+  "dep"
+  "dep.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
