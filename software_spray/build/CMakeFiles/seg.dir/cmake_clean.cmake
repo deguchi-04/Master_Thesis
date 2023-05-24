e52c5e6bb58fb07c2a5fb3d3fefbca7daf9bf34f
Thaidy Deguchi <78708477+deguchi-04@users.noreply.github.com>
2023-05-24 19:02:22 +0100

    Add files via upload

---
+++ software_spray/build/CMakeFiles/seg.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/seg.dir/src/segmentation.cpp.o"
+  "CMakeFiles/seg.dir/src/segmentation.cpp.o.d"
+  "seg"
+  "seg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/seg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
