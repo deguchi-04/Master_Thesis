5d98f14b8b2065159db8f29400aa9363939574f5
Thaidy Deguchi <78708477+deguchi-04@users.noreply.github.com>
2023-06-20 23:57:05 +0100

    Add files via upload

--- software_spray/com.py
+++ software_spray/com.py
@@ -15,75 +15,15 @@ def convert_to_meters(x,y, dist_to_capture, hfov, vfov, img_widht, img_height):
     return x_meters, y_meters
 
 def coordinate_system(x,y,z,hfov, vfov):
-    
-    x_m, y_m = convert_to_meters(x,y, -z, hfov,vfov, 640, 640)
-    x_c, y_c = convert_to_meters(320,320, -z,hfov,vfov, 640, 640)
-    angle_pan =  math.degrees(math.atan((x_c-x_m)/z))
-    tilt_angle = -math.degrees(math.atan((y_c-y_m)/z))  
-    cos_theta = math.cos(angle_pan)
-    sin_theta = math.sin(tilt_angle)
-    
-    ################ COORDINATE CHANGE FROM IMAGE TO PAN ########################
-
-    rotation_img_to_pan = np.array([[1, 0, 0],
-                                    [0, cos_theta, -sin_theta],
-                                    [0, sin_theta, cos_theta]])
-    
-    translation_img_to_pan = np.array([-0.035, 0, -1])
-
-    transformation_matrix_img_to_pan = np.eye(4)
-    transformation_matrix_img_to_pan[:3, :3] = rotation_img_to_pan
-    transformation_matrix_img_to_pan[:3, 3] = translation_img_to_pan
-
-    ################ COORDINATE CHANGE FROM PAN TO TILT ########################
-
-    rotation_pan_to_tilt = np.array([[cos_theta, 0, sin_theta],
-                                     [0, 1, 0],
-                                     [-sin_theta, 0, cos_theta]])
-    
-    translation_pan_to_tilt = np.array([+0.20, 0, 0])
-
-    transformation_matrix_pan_to_tilt = np.eye(4)
-    transformation_matrix_pan_to_tilt[:3, :3] = rotation_pan_to_tilt
-    transformation_matrix_pan_to_tilt[:3, 3] = translation_pan_to_tilt
-
-    ################ COORDINATE CHANGE FROM TILT TO LASER ########################
-
-    rotation_tilt_to_laser = np.eye(3)
-    translation_tilt_to_laser = np.array([+0.035, 0, 0.12])
-
-    transformation_matrix_tilt_to_laser = np.eye(4)
-    transformation_matrix_tilt_to_laser[:3, :3] = rotation_tilt_to_laser
-    transformation_matrix_tilt_to_laser[:3, 3] = translation_tilt_to_laser
-
-    ################ COORDINATE CHANGE FROM IMG TO LASER ########################
-
-    transformation_matrix_img_to_laser = np.dot(np.dot(transformation_matrix_img_to_pan,transformation_matrix_pan_to_tilt),transformation_matrix_tilt_to_laser)
-    point = np.dot(transformation_matrix_img_to_laser,[x,y,0,1])
-    x3 = point[0]
-    y3 = point[1]
-    #z =z+1
     x_m, y_m = convert_to_meters(x,y, -z, hfov, vfov, 640, 640)
     x_c, y_c = convert_to_meters(320,320, -z, hfov, vfov, 640, 640)
-    angle_pan =  math.degrees(math.atan2((x_c-x_m),z))
-    tilt_angle = -math.degrees(math.atan2((y_c-y_m)+0.22,z))  
+    angle_pan =  math.degrees(math.atan2((x_c-x_m)-0.03,z))
+    tilt_angle = -math.degrees(math.atan2((y_c-y_m)+0.18,z))  
     return angle_pan, tilt_angle
 
 
-def main():
-    # Open a serial connection to the Arduino
-    ser = serial.Serial('/dev/ttyACM0', 9600)
-
+def main(flag):
     try:
-        # Add a delay to allow Arduino to initialize
-        time.sleep(2)  # Adjust the delay time as needed
-
-        ser.write(b"start\n")
-        print("Start")
-        flag = 1
-
-        time.sleep(4)  # Adjust the delay time as needed
-
         proc = subprocess.Popen('build/./inf', stdout=subprocess.PIPE)
         pattern = r"Detection: ([\d\.]+),([\d\.]+),([\d\.]+),([\d\.]+),([\d\.]+)"
 
@@ -104,7 +44,7 @@ def main():
 
                 # Check if the ID has already been sent
                 if id_number not in sent_ids:
-                    x3, y3 = coordinate_system(x, y, 1, 34, 36)
+                    x3, y3 = coordinate_system(x, y, 1, 34, 34)
 
                     # Send the coordinates to the Arduino as a string in the format "x,y\n"
                     ser.write(f"{x3:.5f},{y3:.5f},{Lx},{Ly},{id_number},{flag}\n".encode())
@@ -142,4 +82,16 @@ def main():
         proc.kill()
 
 if __name__ == "__main__":
-    main()
+    # Open a serial connection to the Arduino
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    # Add a delay to allow Arduino to initialize
+    time.sleep(2)  # Adjust the delay time as needed
+
+    ser.write(b"stop\n")
+    time.sleep(1)
+    ser.write(b"start\n")
+    print("Start")
+    flag = 1
+
+    time.sleep(4)  # Adjust the delay time as needed
+    main(flag)
